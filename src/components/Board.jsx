9a78d8fc9ccd7076eeaa459244eafb3d620ce362
SiddhesPotdar <siddheshpotdar4787@gmail.com>
2025-05-18 20:53:09 +0530

    feat: add base styling

---
+++ src/components/Board.jsx
@@ -0,0 +1,21 @@
+import Square from "./Square";
+
+const Board = () => {
+  return (
+    <div className="board">
+      <Square />
+      <Square />
+      <Square />
+
+      <Square />
+      <Square />
+      <Square />
+
+      <Square />
+      <Square />
+      <Square />
+    </div>
+  );
+};
+
+export default Board;
---
+++ src/components/Square.jsx
@@ -0,0 +1,5 @@
+const Square = () => {
+  return <button className="square">X</button>;
+};
+
+export default Square;
