bb497f9ee9aa1507c88f5093341289dd73c17d56
SiddhesPotdar <siddheshpotdar4787@gmail.com>
2025-05-18 22:36:52 +0530

    fix: level up states to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,54 @@
+import { useState } from "react";
 import Board from "./components/Board";
 
 const App = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(true);
+
+  const checkWinner = () => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[b] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  const handleClick = (i) => {
+    if (squares[i] || checkWinner()) return;
+    const nextSquares = squares.slice();
+    isXNext ? (nextSquares[i] = "X") : (nextSquares[i] = "O");
+    setSquares(nextSquares);
+    setIsXNext(!isXNext);
+  };
+
+  const winner = checkWinner();
+  let status;
+  if (winner) {
+    status = "💥Winner: " + winner + " 🎉";
+  } else {
+    status = "Next player: " + (isXNext ? "X" : "O");
+  }
+
   return (
     <div>
-      <Board />
+      <div className="status">{status}</div>
+      <Board squares={squares} handleClick={handleClick} />
     </div>
   );
 };
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,65 +1,18 @@
-import { useState } from "react";
 import Square from "./Square";
 
-const Board = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [isXNext, setIsXNext] = useState(true);
-
-  const checkWinner = () => {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (
-        squares[a] &&
-        squares[a] === squares[b] &&
-        squares[b] === squares[c]
-      ) {
-        return squares[a];
-      }
-    }
-    return null;
-  };
-
-  const handleClick = (i) => {
-    if (squares[i] || checkWinner()) return;
-    const nextSquares = squares.slice();
-    isXNext ? (nextSquares[i] = "X") : (nextSquares[i] = "O");
-    setSquares(nextSquares);
-    setIsXNext(!isXNext);
-  };
-
-  const winner = checkWinner();
-  let status;
-  if (winner) {
-    status = "💥Winner: " + winner + " 🎉";
-  } else {
-    status = "Next player: " + (isXNext ? "X" : "O");
-  }
-
+const Board = ({ squares, handleClick }) => {
   return (
-    <>
-      <div className="status">{status}</div>
-      <div className="board">
-        <Square value={squares[0]} onClick={() => handleClick(0)} />
-        <Square value={squares[1]} onClick={() => handleClick(1)} />
-        <Square value={squares[2]} onClick={() => handleClick(2)} />
-        <Square value={squares[3]} onClick={() => handleClick(3)} />
-        <Square value={squares[4]} onClick={() => handleClick(4)} />
-        <Square value={squares[5]} onClick={() => handleClick(5)} />
-        <Square value={squares[6]} onClick={() => handleClick(6)} />
-        <Square value={squares[7]} onClick={() => handleClick(7)} />
-        <Square value={squares[8]} onClick={() => handleClick(8)} />
-      </div>
-    </>
+    <div className="board">
+      <Square value={squares[0]} onClick={() => handleClick(0)} />
+      <Square value={squares[1]} onClick={() => handleClick(1)} />
+      <Square value={squares[2]} onClick={() => handleClick(2)} />
+      <Square value={squares[3]} onClick={() => handleClick(3)} />
+      <Square value={squares[4]} onClick={() => handleClick(4)} />
+      <Square value={squares[5]} onClick={() => handleClick(5)} />
+      <Square value={squares[6]} onClick={() => handleClick(6)} />
+      <Square value={squares[7]} onClick={() => handleClick(7)} />
+      <Square value={squares[8]} onClick={() => handleClick(8)} />
+    </div>
   );
 };
 
